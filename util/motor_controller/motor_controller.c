61b57b2f61e71594058f18beba4e44bebe7613e4
davidlane23 <2201898@sit.singaporetech.edu.sg>
2023-10-28 21:37:23 +0800

    updated motor controller
    updated motor controller, should be fully working

--- util/motor_controller/motor_controller.c
+++ util/motor_controller/motor_controller.c
@@ -14,8 +14,8 @@
 #define L_CLOCKWISE_PIN 11
 
 #define RIGHT_MOTOR 21
-#define R_REVERSE_PIN 3
-#define R_CLOCKWISE_PIN 2
+#define R_REVERSE_PIN 2
+#define R_CLOCKWISE_PIN 3
 
 
 void initialize_gpio_pins()
@@ -95,34 +95,6 @@ void reverse()
     gpio_put(R_REVERSE_PIN,1);
 }
 
-// Enables the car to make a left turn
-//
-void turn_left(uint *l_slice_num,uint *r_slice_num)
-{
-    update_speed(l_slice_num,PWM_CHAN_A,0.3);
-    update_speed(r_slice_num,PWM_CHAN_B,0.8);
-    // Checks the current state of the left wheel, sets to the opposite to counteract right wheel movement
-    //
-    char clockwise_state = gpio_get(L_CLOCKWISE_PIN);
-    char reverse_state = gpio_get(L_REVERSE_PIN);
-    gpio_put(L_CLOCKWISE_PIN, !clockwise_state);
-    gpio_put(L_REVERSE_PIN, !reverse_state);
-}
-
-// Enables the car to make a right turn
-//
-void turn_right(uint *l_slice_num,uint *r_slice_num)
-{
-    update_speed(l_slice_num,PWM_CHAN_A,0.8);
-    update_speed(r_slice_num,PWM_CHAN_B,0.3);
-    // Checks the current state of the right wheel, sets to the opposite to counteract left wheel movement
-    //
-    char clockwise_state = gpio_get(R_CLOCKWISE_PIN);
-    char reverse_state = gpio_get(R_REVERSE_PIN);
-
-    gpio_put(R_CLOCKWISE_PIN, !clockwise_state);
-    gpio_put(R_REVERSE_PIN, !reverse_state);
-}
 // Resets the speed back to default
 //
 void reset_speed(uint *l_slice_num,uint *r_slice_num)
@@ -130,56 +102,72 @@ void reset_speed(uint *l_slice_num,uint *r_slice_num)
     update_speed(l_slice_num,PWM_CHAN_A,0.5);
     update_speed(r_slice_num,PWM_CHAN_B,0.5);    
 }
-
-// void clockwise_motor(char motor_direction)
-// {   if(motor_direction == 'L')
-//     {
-//         gpio_set_pulls(L_CLOCKWISE_PIN,true,false);
-//         gpio_set_pulls(L_REVERSE_PIN,false,true);
-//         enable_motor(LEFT_MOTOR);
-//     }
-//     else
-//     {
-//         gpio_set_pulls(R_CLOCKWISE_PIN,true,false);
-//         gpio_set_pulls(R_REVERSE_PIN,false,true);
-//         enable_motor(RIGHT_MOTOR);
-//     }
-// }
-// void reverse_motor(char motor_direction)
-// {
-//     if(motor_direction == 'L')
-//     {
-//         gpio_set_pulls(L_CLOCKWISE_PIN,false,true);
-//         gpio_set_pulls(L_REVERSE_PIN,true,false);
-//         enable_motor(LEFT_MOTOR);
-//     }
-//     else
-//     {
-//         gpio_set_pulls(R_CLOCKWISE_PIN,false,true);
-//         gpio_set_pulls(R_REVERSE_PIN,true,false);
-//         enable_motor(RIGHT_MOTOR);       
-//     }
-// }
-
 // Enables the car to stop
 //
 void stop_motors(uint *l_slice_num,uint *r_slice_num)
 {
-    // uint l_slice_num = pwm_gpio_to_slice_num(LEFT_MOTOR);
-    // uint r_slice_num = pwm_gpio_to_slice_num(RIGHT_MOTOR);
+    pwm_set_chan_level(*l_slice_num,PWM_CHAN_A,0);
+    pwm_set_chan_level(*r_slice_num,PWM_CHAN_B,0);
+}
 
-    // if(motor_dir == 'L')
+// Enables the car to make a left turn
+//
+void turn_left(uint *l_slice_num,uint *r_slice_num,uint reverse_turn)
+{
+    update_speed(l_slice_num,PWM_CHAN_A,0.3);
+    update_speed(r_slice_num,PWM_CHAN_B,0.8);
+    if(!reverse_turn)
+    {
+        gpio_put(L_CLOCKWISE_PIN, 0);
+        gpio_put(L_REVERSE_PIN, 1);
+        gpio_put(R_CLOCKWISE_PIN, 1);
+        gpio_put(R_REVERSE_PIN, 0);
+    }
+    else
+    {
+        gpio_put(L_CLOCKWISE_PIN, 1);
+        gpio_put(L_REVERSE_PIN, 0);
+        gpio_put(R_CLOCKWISE_PIN, 0);
+        gpio_put(R_REVERSE_PIN, 1);
+    }
+}
+
+// Enables the car to make a right turn
+//
+void turn_right(uint *l_slice_num,uint *r_slice_num,uint reverse_turn)
+{
+    update_speed(l_slice_num,PWM_CHAN_A,0.8);
+    update_speed(r_slice_num,PWM_CHAN_B,0.3);
+    if(!reverse_turn)
+    {
+        gpio_put(L_CLOCKWISE_PIN, 1);
+        gpio_put(L_REVERSE_PIN, 0);
+        gpio_put(R_CLOCKWISE_PIN, 0);
+        gpio_put(R_REVERSE_PIN, 1);
+    }
+    else
+    {
+        gpio_put(L_CLOCKWISE_PIN, 0);
+        gpio_put(L_REVERSE_PIN,1);
+        gpio_put(R_CLOCKWISE_PIN, 1);
+        gpio_put(R_REVERSE_PIN, 0);
+    }
+    // Checks if a reverse turn is specified, if not specified, forward turn right else, reverse turn right 
+    //
+    // if(!reverse_turn)
     // {
-    //     channel = PWM_CHAN_A;   
+    //     gpio_put(L_CLOCKWISE_PIN, 1);n
+    //     gpio_put(L_REVERSE_PIN, 0);
+    //     gpio_put(R_CLOCKWISE_PIN, 0);
+    //     gpio_put(R_REVERSE_PIN, 1);
     // }
     // else
     // {
-    //     channel = PWM_CHAN_B;
+    //     gpio_put(L_CLOCKWISE_PIN, 1);
+    //     gpio_put(L_REVERSE_PIN, 0);
+    //     gpio_put(R_CLOCKWISE_PIN, 0);
+    //     gpio_put(R_REVERSE_PIN, 1);
     // }
-    // printf("Channel: %s\n",channel);
-    pwm_set_chan_level(*l_slice_num,PWM_CHAN_A,0);
-    pwm_set_chan_level(*r_slice_num,PWM_CHAN_B,0);
-    
 }
 
 int main() 
@@ -194,7 +182,7 @@ int main()
     //
     uint l_slice_num = get_slice_num(LEFT_MOTOR);
     uint r_slice_num = get_slice_num(RIGHT_MOTOR);
-
+    uint reverse_turn = 0;
     // Start up the motors
     //
     enable_motors(&l_slice_num,&r_slice_num);
@@ -216,17 +204,21 @@ int main()
         {
             case 'f':
             {
+                reset_speed(&l_slice_num,&r_slice_num);
                 move_forward();
                 break;
             }
             case 'l':
             {
-                turn_left(&l_slice_num,&r_slice_num);
+                reset_speed(&l_slice_num,&r_slice_num);
+                reverse_turn = 0;
+                turn_left(&l_slice_num,&r_slice_num,reverse_turn);
                 break;
             }
             case 't':
             {
-                turn_right(&l_slice_num,&r_slice_num);
+                reverse_turn = 0;
+                turn_right(&l_slice_num,&r_slice_num,reverse_turn);
                 break;
             }
             case 's':
@@ -235,9 +227,20 @@ int main()
                 break;
             }
             case 'r':
+                update_speed(&l_slice_num,PWM_CHAN_A,0.5);
+                update_speed(&r_slice_num,PWM_CHAN_B,0.5);
                 reverse();
                 break;
+            case 'e':
+                reverse_turn = 1;
+                turn_left(&r_slice_num,&l_slice_num,reverse_turn);
+                break;
+            case 'j':
+                reverse_turn = 1;
+                turn_right(&r_slice_num,&l_slice_num,reverse_turn);
+                break;
             default:
+                reset_speed(&l_slice_num,&r_slice_num);
                 stop_motors(&l_slice_num,&r_slice_num);
         }
     }
