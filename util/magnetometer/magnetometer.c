693febb303d4782d86fbd62a10d09c4a9c5689df
DerrickLJH <dljh1234@gmail.com>
2023-10-30 02:37:38 +0800

    Accelerometer calibrated and done

--- util/magnetometer/magnetometer.c
+++ util/magnetometer/magnetometer.c
@@ -4,10 +4,11 @@
 
 #define I2C_PORT i2c0
 
-#define ACC_ADDRESS 0x19 // Address for accelerometer
-#define MAG_ADDRESS 0x1E // Address for magnetometer
+#define ACC_ADDRESS 0x19
+#define MAG_ADDRESS 0x1E
 
 #define CTRL_REG1_A 0x20
+#define STATUS_REG_A 0x27
 #define OUT_X_L_A 0x28
 #define OUT_X_H_A 0x29
 #define OUT_Y_L_A 0x2A
@@ -26,6 +27,10 @@
 
 #define PIN_SDA 0
 #define PIN_SCL 1
+
+int16_t bias_x = 0;
+int16_t bias_y = 0;
+int16_t bias_z = 0;
 
 void initI2C() {
     i2c_init(I2C_PORT, 400000);
@@ -47,13 +52,13 @@ uint8_t readRegister(uint8_t address, uint8_t reg) {
 }
 
 void configureAccelerometer() {
-    writeRegister(ACC_ADDRESS, CTRL_REG1_A, 0x47); // Enable accelerometer, 100Hz data rate
+    writeRegister(ACC_ADDRESS, CTRL_REG1_A, STATUS_REG_A);
 }
 
 void readAccelerometerData(int16_t* x, int16_t* y, int16_t* z) {
-    *x = (u_int16_t)((readRegister(ACC_ADDRESS, OUT_X_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_X_L_A));
-    *y = (u_int16_t)((readRegister(ACC_ADDRESS, OUT_Y_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_Y_L_A));
-    *z = (u_int16_t)((readRegister(ACC_ADDRESS, OUT_Z_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_Z_L_A));
+    *x = (int16_t)((readRegister(ACC_ADDRESS, OUT_X_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_X_L_A)) - bias_x;
+    *y = (int16_t)((readRegister(ACC_ADDRESS, OUT_Y_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_Y_L_A)) - bias_y;
+    *z = (int16_t)((readRegister(ACC_ADDRESS, OUT_Z_H_A) << 8) | readRegister(ACC_ADDRESS, OUT_Z_L_A)) - bias_z;
 }
 
 void configureMagnetometer() {
@@ -61,9 +66,38 @@ void configureMagnetometer() {
 }
 
 void readMagnetometerData(int16_t* x, int16_t* y, int16_t* z) {
-    *x = (u_int16_t)((readRegister(MAG_ADDRESS, OUT_X_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_X_L_M));
-    *y = (u_int16_t)((readRegister(MAG_ADDRESS, OUT_Y_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_Y_L_M));
-    *z = (u_int16_t)((readRegister(MAG_ADDRESS, OUT_Z_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_Z_L_M));
+    *x = (int16_t)((readRegister(MAG_ADDRESS, OUT_X_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_X_L_M));
+    *y = (int16_t)((readRegister(MAG_ADDRESS, OUT_Y_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_Y_L_M));
+    *z = (int16_t)((readRegister(MAG_ADDRESS, OUT_Z_H_M) << 8) | readRegister(MAG_ADDRESS, OUT_Z_L_M));
+}
+
+void calibrateAccelerometer() {
+    const int numSamples = 100;  
+    int16_t x_accum = 0;
+    int16_t y_accum = 0;
+    int16_t z_accum = 0;
+
+    for (int i = 0; i < numSamples; i++) {
+        int16_t x, y, z;
+        readAccelerometerData(&x, &y, &z);
+        x_accum += x;
+        y_accum += y;
+        z_accum += z;
+        sleep_ms(10);  
+    }
+
+    bias_x = x_accum / numSamples;
+    bias_y = y_accum / numSamples;
+    bias_z = z_accum / numSamples;
+}
+
+void calculateAcceleration(int16_t x, int16_t y, int16_t z) {
+    double g = 9.81; // Standard gravity in m/s^2
+    double acc_x = (double)x * (g / 16384.0);
+    double acc_y = (double)y * (g / 16384.0);
+    double acc_z = (double)z * (g / 16384.0);
+
+    printf("Acceleration: (X = %.2f m/s^2, Y = %.2f m/s^2, Z = %.2f m/s^2)\n", acc_x, acc_y, acc_z);
 }
 
 int main() {
@@ -71,6 +105,7 @@ int main() {
     initI2C();
     configureAccelerometer();
     configureMagnetometer();
+    calibrateAccelerometer();
 
     while (1) {
         int16_t x_acc, y_acc, z_acc;
@@ -79,7 +114,8 @@ int main() {
         readAccelerometerData(&x_acc, &y_acc, &z_acc);
         readMagnetometerData(&x_mag, &y_mag, &z_mag);
 
-        printf("Accelerometer Data: (X = %d, Y = %d, Z = %d)\n", x_acc, y_acc, z_acc);
+        calculateAcceleration(x_acc, y_acc, z_acc);
+        // printf("Accelerometer Data: (X = %d, Y = %d, Z = %d)\n", x_acc, y_acc, z_acc);
         printf("Magnetometer Data: (X = %d, Y = %d, Z = %d)\n", x_mag, y_mag, z_mag);
 
         sleep_ms(500);
