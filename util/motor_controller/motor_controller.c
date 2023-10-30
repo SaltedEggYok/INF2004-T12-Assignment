4821334b98d9d3feb0eaca2e38a2407fba1fd5fc
davidlane23 <2201898@sit.singaporetech.edu.sg>
2023-10-30 15:15:09 +0800

    update comments change shit
    update comments change shit

--- util/motor_controller/motor_controller.c
+++ util/motor_controller/motor_controller.c
@@ -9,13 +9,23 @@
 
 #define CLK_CYCLE_NO 62500
 
-#define LEFT_MOTOR 20
-#define L_REVERSE_PIN 10
-#define L_CLOCKWISE_PIN 11
 
-#define RIGHT_MOTOR 21
-#define R_REVERSE_PIN 2
-#define R_CLOCKWISE_PIN 3
+// Setup 
+// EN A - 21
+// EN B - 20
+// N1 - 11
+// N2 - 10
+// N3 - 2
+// N4 - 3
+// MOTOR WIRE RED NEARER THE 3 PINS SIDE (VBUS SIDE)
+// when car facing forward , right side - right 2 
+#define LEFT_MOTOR 21
+#define L_REVERSE_PIN 2
+#define L_CLOCKWISE_PIN 3
+
+#define RIGHT_MOTOR 20
+#define R_REVERSE_PIN 10
+#define R_CLOCKWISE_PIN 11
 
 
 void initialize_gpio_pins()
